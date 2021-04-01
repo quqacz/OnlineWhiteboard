4e32e1ee91f7de55a410c1bf7988ae303f6d1ba4
quqacz <67021265+quqacz@users.noreply.github.com>
2021-04-01 18:47:35 +0200

    autoscale canvas

--- public/scripts/board.js
+++ public/scripts/board.js
@@ -20,20 +20,35 @@ gumka.addEventListener('click', ()=>{
     removeActiveStyle()
     gumka.classList.add('active');
 })
+
 rysik.addEventListener('click', ()=>{
     settings.tool = 'RYSIK';
     removeActiveStyle()
     rysik.classList.add('active');
 })
+
 linia.addEventListener('click', ()=>{
     settings.tool = 'LINIA';
     removeActiveStyle()
     linia.classList.add('active');
 })
 
-ctx.fillRect(0,0, canvas.width, canvas.height);
+window.addEventListener('resize', ()=>{
+    resizeCanvas();
+})
+
+resizeCanvas();
 
 function removeActiveStyle(){
     for(tool of toolbarControls)
         tool.classList.remove('active');
+}
+
+function resizeCanvas(){
+    const ratio = 827/1575;
+    let parentW = canvas.parentNode.clientWidth-12;
+    let height = ratio * parentW;
+    console.log(`${parentW}: ${height}`);
+    canvas.width = parentW;
+    canvas.height = height;
 }
