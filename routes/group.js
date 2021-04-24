a8e59cff09660b0a2e4c703a1317bae256836544
quqacz <67021265+quqacz@users.noreply.github.com>
2021-04-24 15:18:10 +0200

    add lesson bug fix

--- routes/group.js
+++ routes/group.js
@@ -56,4 +56,21 @@ router.get('/:id/lesson/:lessonId', isLoggedIn, async(req, res)=>{
 	res.render('board', {groupId: req.params.lessonId, owner: groupOwner.owner._id, messages: messages.messages})
 })
 
+router.post('/:id/lesson/add', isLoggedIn, async(req, res)=>{
+    try{
+        const { topic } = req.body;
+        const newLesson = new Lesson({
+            topic
+        })
+        newLesson.save();
+        const group = await Group.findOne({_id: req.params.id});
+        group.lessons.push(newLesson);
+        const updatedGroup = await group.save();
+        res.redirect('/group/'+req.params.id);
+    }catch (e){
+        console.log(e)
+        res.redirect('/group/'+req.params.id);
+    }
+})
+
 module.exports = router;
--- routes/lesson.js
+++ routes/lesson.js
@@ -9,21 +9,4 @@ router.get('/add', isLoggedIn, (req,res)=>{
     res.render('newLesson', {groupId: req.params.id});
 })
 
-router.post('/add', isLoggedIn, async(req, res)=>{
-    try{
-        const { topic } = req.body;
-        const newLesson = new Lesson({
-            topic
-        })
-        newLesson.save();
-        const group = await Group.findOne({_id: req.params.id});
-        group.lessons.push(newLesson);
-        const updatedGroup = await group.save();
-        res.redirect('/group/'+req.params.id);
-    }catch (e){
-        console.log(e)
-        res.redirect('/group/'+req.params.id);
-    }
-})
-
 module.exports = router;
