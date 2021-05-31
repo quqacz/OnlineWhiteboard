6592e4ae747c6e586e18affc3312a27d2a9a6538
Adrian Kunikowski <kunikowskiadrian@onet.pl>
2021-05-31 21:09:39 +0200

    Change to validation approach
    Validation now shows all errors to the user and labels them using colors - if an input is red, it means he needs to correct the error there.

--- public/scripts/validation.js
+++ public/scripts/validation.js
@@ -1,48 +1,81 @@
 function validateDataChange() {
 			const imie = document.getElementById('newName').value
 			const nazwisko = document.getElementById('newLastName').value
+			var RegCheck=0
 			var RegImie = /^[A-ZĄĆĘŁŃÓŚŹŻ]{1}[a-ząćęłńóśźż]+$/;
 			var RegNazw = /^([A-ZĄĆĘŁŃÓŚŹŻ]{1}[a-ząćęłńóśźż]+){1}(-{1}[A-ZĄĆĘŁŃÓŚŹŻ]{1}[a-ząćęłńóśźż]+){0,1}$/;
+			document.getElementById('ValidateImieInfo').innerHTML=''
+			document.getElementById('ValidateNazwInfo').innerHTML=''
+			document.getElementById('newName').style.backgroundColor=''
+			document.getElementById('newLastName').style.backgroundColor=''
 			if(!RegImie.test(imie))
 			{
-				alert("W imieniu występuje błąd. Upewnij się, że zaczyna się ono z dużej litery i nie ma żadnych spacji");
-				return false;
+				document.getElementById('newName').style.backgroundColor = '#F26760';
+				document.getElementById('ValidateImieInfo').innerHTML='W imieniu występuje błąd. Upewnij się, że zaczyna się ono z dużej litery i nie ma żadnych spacji';
+				RegCheck=1
 			}
-			else if(!RegNazw.test(nazwisko))
+			if(!RegNazw.test(nazwisko))
 			{
-				alert("W nazwisku występuje błąd. Upewnij sie, że zaczyna się ono z dużej litery, a jeśli jest podwójne, to sprawdź czy jest oddzielone myślnikiem (np. Kowalski-Kmicic)");
-				return false;
+				document.getElementById('newLastName').style.backgroundColor = '#F26760';
+				document.getElementById('ValidateNazwInfo').innerHTML='W nazwisku występuje błąd. Upewnij sie, że zaczyna się ono z dużej litery, a jeśli jest podwójne, to sprawdź czy jest oddzielone myślnikiem (np. Kowalski-Kmicic)';
+				RegCheck=1
 			}
+			if(RegCheck==0)
 			return true;
+			
+			return false;
 }
 function validatePassChange(){
 			const haslo = document.getElementById('newPassword1').value
 			const haslo2 = document.getElementById('newPassword2').value
+			var RegCheck=0
+			document.getElementById('ValidatePassInfo').innerHTML=''
+			document.getElementById('ValidatePass2Info').innerHTML=''	
+			document.getElementById('newPassword1').style.backgroundColor=''
+			document.getElementById('newPassword2').style.backgroundColor=''
 			if(haslo.includes(' '))
 			{
-				alert("Haslo nie może mieć spacji");
-				return false;
+				document.getElementById('ValidatePassInfo').innerHTML='Haslo nie może mieć spacji';
+				document.getElementById('newPassword1').style.backgroundColor='#F26760'
+				RegCheck=1
 			}
-			else if(haslo!=haslo2)
+			if(haslo!=haslo2)
 			{
-				alert("Hasla sa różne");
-				return false;
+				document.getElementById('ValidatePass2Info').innerHTML='Hasła są różne';
+				document.getElementById('newPassword1').style.backgroundColor='#F26760'
+				document.getElementById('newPassword2').style.backgroundColor='#F26760'
+				RegCheck=1
 			}
+			if(RegCheck==0)
 			return true;
+			
+			return false;
 }
 function validateLogin()
 {
+		document.getElementById('ValidateLoginInfo').innerHTML=''
+		document.getElementById('login').style.backgroundColor=''
 		const login = document.getElementById('login').value 
 		if(login.includes(' '))
 			{
-				alert("Login nie może mieć spacji");
+				document.getElementById('ValidateLoginInfo').innerHTML='Login nie może mieć spacji';
+				document.getElementById('login').style.backgroundColor='#F26760'
 				return false;
 			}
 		return true;	
 }
-function validateTest()
+function validateRegister()
 {
-	if(validateDataChange() && validateLogin() && validatePassChange() )
+	var RegCheck=0
+	if(!validateDataChange())
+	RegCheck=1
+	if(!validateLogin())
+	RegCheck=1
+	if(!validatePassChange())
+	RegCheck=1
+
+		if(RegCheck==0)
 		return true;
-	return false;
+			
+		return false;
 }
